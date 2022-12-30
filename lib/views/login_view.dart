52e197e197fff73760e0a073bb1f7dd9ddf65d22
Mirzad Sadovic <mirzadsadovic@gmail.com>
2022-12-30 09:21:38 +0100

    step-5

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
+
+import 'package:mynotes/constants/routes.dart';
 //"show log" (mean only that func (log))
 //"as devtools" (is alias only, for knowing which func. is?)
 
@@ -63,7 +65,7 @@ class _LoginViewState extends State<LoginView> {
                   password: password,
                 );
                 Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/notes/',
+                  notesRoute,
                   (route) => false,
                 );
               } on FirebaseAuthException catch (e) {
@@ -79,7 +81,7 @@ class _LoginViewState extends State<LoginView> {
           TextButton(
             onPressed: () {
               Navigator.of(context).pushNamedAndRemoveUntil(
-                '/register/',
+                registerRoute,
                 (route) => false,
               );
             },
