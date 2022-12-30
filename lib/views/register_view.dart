952e5d1cfc6bcbdbfac963418e716143ff946efc
Mirzad Sadovic <mirzadsadovic@gmail.com>
2022-12-30 09:59:16 +0100

    step-6

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
+//"show log" (mean only that func (log))
+//"as devtools" (is alias only, for knowing which func. is?)
 
 import 'package:mynotes/constants/routes.dart';
 
